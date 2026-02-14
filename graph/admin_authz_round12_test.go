f952f8819720aa37942a326bc4cf9d282d9d8bdc
Aron Price <aron23@gmail.com>
2026-02-14 11:43:48 -0500

    graph: updateAdminAgentPolicy requires admin scope (#107)

--- graph/admin_authz_round12_test.go
+++ graph/admin_authz_round12_test.go
@@ -1,9 +1,12 @@
 package graph
 
 import (
+	"context"
 	"testing"
 
 	"github.com/equaltoai/lesser/graph/model"
+	"github.com/equaltoai/lesser/pkg/auth"
+	"github.com/equaltoai/lesser/pkg/common"
 	apperrors "github.com/equaltoai/lesser/pkg/errors"
 	"github.com/stretchr/testify/require"
 )
@@ -22,5 +25,36 @@ func TestAdminAccountsRequiresAdmin_Round12(t *testing.T) {
 	_, err = qry.AdminAccounts(round12AuthContext("bob"), &first, nil)
 	require.Error(t, err)
 	require.True(t, apperrors.HasCode(err, apperrors.CodeForbidden))
+	require.Equal(t, 403, apperrors.GetHTTPStatus(err))
+}
+
+func TestUpdateAdminAgentPolicyRequiresAdminScope_Round12(t *testing.T) {
+	resolver, _ := newRound12GraphResolver(t)
+
+	mut := resolver.Mutation()
+
+	ctx := context.WithValue(context.Background(), common.ContextKeyClaims, &auth.Claims{
+		Username: "bob",
+		Scopes:   []string{"read"},
+	})
+
+	_, err := mut.UpdateAdminAgentPolicy(ctx, model.UpdateAdminAgentPolicyInput{
+		AllowAgents:                    true,
+		AllowAgentRegistration:         false,
+		DefaultQuarantineDays:          0,
+		MaxAgentsPerOwner:              0,
+		AllowRemoteAgents:              false,
+		RemoteQuarantineDays:           0,
+		BlockedAgentDomains:            nil,
+		TrustedAgentDomains:            nil,
+		AgentMaxPostsPerHour:           0,
+		VerifiedAgentMaxPostsPerHour:   0,
+		AgentMaxFollowsPerHour:         0,
+		VerifiedAgentMaxFollowsPerHour: 0,
+		HybridRetrievalEnabled:         false,
+		HybridRetrievalMaxCandidates:   0,
+	})
+	require.Error(t, err)
+	require.True(t, apperrors.HasCode(err, apperrors.CodeInsufficientScope))
 	require.Equal(t, 403, apperrors.GetHTTPStatus(err))
 }
